ef2b0c18f603dad16b1cf64e9884d7d58175ee2b
Jefferson Silva <jeffersondasilva14@gmail.com>
2014-04-04 09:30:15 -0300

    commit de crud menos delet dos clientes

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -28,30 +28,20 @@ class ClientesController < ApplicationController
 		")
 		@teste = Cliente.find( :last )
 				flash[:notice] = "Cliente slavo com sucesso!"
-				redirect_to action: 'lista' 
+				redirect_to action: 'listar' 
 			
 	end
 	def listar
 		@lista_clientes = Cliente.find_by_sql("
 			SELECT
 				clientes.id,
-				clientes.nome,
-				logradouros.endereco,
-				clientes.endereco_n,
-				logradouros.bairro,
-				logradouros.cidade,
-				logradouros.uf,
-				clientes.telefone1,
-				clientes.telefone2,
-				clientes.telefone3,
-				clientes.celular,
-				clientes.rg,
+				clientes.nome,				
 				tipos_cliente.nome tp_cliente,
 				clientes.status,
+				clientes.dt_cadastro,
 				clientes.ultima_alteracao
 			FROM
 				clientes
-				INNER JOIN logradouros ON logradouros.id = clientes.endereco_id
 				INNER JOIN tipos_cliente ON tipos_cliente.id = clientes.tp_cliente_id
 
 			ORDER BY
@@ -76,6 +66,7 @@ class ClientesController < ApplicationController
 				clientes.rg,
 				tipos_cliente.nome tp_cliente,
 				clientes.status,
+				clientes.dt_cadastro,
 				clientes.ultima_alteracao
 			FROM
 				clientes
@@ -97,13 +88,22 @@ class ClientesController < ApplicationController
 				logradouros
 		")
 		
+		@editar_cliente = Cliente.find_by_sql ("SELECT * FROM clientes WHERE id = #{params[:id]}")
+	end
+
+	def update
 		@editar_cliente = Cliente.find_by_sql("
-				SELECT
-					 nome, endereco_id , endereco_n , telefone1 , telefone2 , telefone3 , celular , rg , tp_cliente_id
-				FROM
-					clientes
-				WHERE
-					id = #{params[:id]} 
-			")			
+			UPDATE 
+				clientes
+			SET
+				nome = '#{params[:cliente][:nome]}', endereco_id = '#{params[:cliente][:endereco_id]}', endereco_n = '#{params[:cliente][:endereco_n]}', telefone1 = '#{params[:cliente][:telefone1]}',
+					telefone2 = '#{params[:cliente][:telefone2]}', telefone3 = '#{params[:cliente][:telefone3]}', celular = '#{params[:cliente][:celular]}', rg = '#{params[:cliente][:rg]}' ,
+					 tp_cliente_id = '#{params[:cliente][:tp_cliente_id]}' , status = '#{params[:cliente][:status]}', ultima_alteracao = '#{Time.now}'
+			WHERE 
+				clientes.id = #{params[:id]}
+
+		")
+		flash[:notice] = "Cliente alterado com sucesso"
+		redirect_to action: 'editar' , id: params[:id]
 	end
 end
